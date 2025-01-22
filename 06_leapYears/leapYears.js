702e44b57f926a90e1fc3f9cf62bbb0cd850f327
inspiritace <inspiritace@gmail.com>
2025-01-21 23:08:44 -0600

    added js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,9 @@
-const leapYears = function() {
+const leapYears = function(leapYears) {
+  if (leapYears % 4 === 0 && leapYears % 100 !== 0 || leapYears % 400 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 
 };
 
