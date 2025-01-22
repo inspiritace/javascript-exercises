28cddab3f8337918ec79c423ee08ec845aad43fe
inspiritace <inspiritace@gmail.com>
2025-01-21 23:18:02 -0600

    final script

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(convertToCelsius) {
+  var celsius = (convertToCelsius - 32) * 5 / 9;
+  return celsius = Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(convertToFahrenheit) {
+  var fahrenheit = convertToFahrenheit * 9 / 5 + 32;
+  return fahrenheit = Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
