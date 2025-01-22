56609689b006ae69f3ddfcbcff39e5feeb4adc41
inspiritace <inspiritace@gmail.com>
2025-01-21 23:26:48 -0600

    calculator js final

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,34 @@
-const add = function() {
+const add = function(add1,add2) {
+  return add1 + add2;
+};
+
+const subtract = function(subtract1,subtract2) {
+  return subtract1 - subtract2;
 	
 };
 
-const subtract = function() {
+const sum = function(sumArray) {
+  return sumArray.reduce((total, current) => total + current, 0);
 	
 };
 
-const sum = function() {
+const multiply = function(multiplyArray) {
+  return multiplyArray.reduce((product, current) => product * current);
+
+};
+
+const power = function(power1,power2) {
+  return Math.pow(power1, power2);
 	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(factorial) {
+  if (factorial === 0) return 1;
+  let result = 1;
+  for (let i = factorial; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 	
 };
 
