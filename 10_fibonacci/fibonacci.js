5882db1cb0fe36218826e3cd150e987d4c2332dc
inspiritace <inspiritace@gmail.com>
2025-01-25 22:36:46 -0600

    initial fibonacci js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n === 0) return 0;
+    if (n === 1) return 1;
 
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++) {
+        const c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
